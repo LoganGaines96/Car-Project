e4c9f7995d4e3daa3ab0735e0b57f1d0145595d1
LoganGaines96 <66852313+LoganGaines96@users.noreply.github.com>
2020-07-13 19:36:58 -0500

    added Schedule1 and Cancel

--- Dealer_sc.rb
+++ Dealer_sc.rb
@@ -1,55 +1,36 @@
-puts "Welcome! What is your name?"
-name = gets.chomp 
+class Schedule
+    attr_accessor :name, :vehicle, :request, :chosen_slot
+     def initialize(name, vehicle, request, chosen_slot)
+         @name = name
+         @vehicle = vehicle
+         @request = request
+         @chosen_slot = chosen_slot
+     end
 
-puts "What vehicle do you have?"
-vehicle = gets.chomp
+     def message()
+         puts "#{@name}, you have an appointment for #{@request} on #{@vehicle} at #{@chosen_slot}."
+     end
 
-puts "What work needs to be done?"
-    request = gets.chomp
+     def cancel()
+         puts "sorry #{@name} your appointment has been canceled."
+     end 
+end
+ puts "Welcome! What is your name?"
+ name = gets.chomp
+ puts "What vehicle do you have?"
+ vehicle = gets.chomp
+ puts "What work needs to be done?"
+ request = gets.chomp
+ @slots = ["1:00", "2:00", "3:00", "4:00"]
+     puts "There are #{@slots.size} slots available:"
+     @slots.each do |slot|
+         puts slot
+     end
 
-    @info = [name, vehicle, request]
-    puts "Your information is: "
-    @info.each do |info|
-        puts info
-    end
+ puts "What time slot do you want?"
+ chosen_slot = gets.chomp
 
-@slots = ["1:00", "2:00", "3:00", "4:00"]
-# puts @slots
-    puts "There are #{@slots.size} time slots available:"
-    @slots.each do |slot|
-        puts slot
-    end
+ Schedule1 = Schedule.new(name, vehicle, request, chosen_slot)
+ Schedule1.message 
+ Schedule1.cancel 
 
-puts "What time slot do you want?"
-    chosen_slot = gets.chomp
-    puts "#{name}, Your have an appointment for #{request} on #{vehicle} at #{chosen_slot}"
-
-# def (car.new)
-#     puts "#{car.capatilize}."
-#   end
-
-#   def (car.make)
-#     puts "#{make.capatilize}."
-#   end
-
-#   def (car.model)
-#     puts "#{model.capatilize}."
-#   end
-
-#   def (car.year)
-#     puts "#{year.capatilize}."
-#   end
-
-#   def (car.color)
-#     puts "#{color.capatilize}."
-#   end
-
-# puts "This is the make and model for customer vehicle"
-
-# customer_car.make = "#"
-# customer_car.model = "#"
-# customer_car.year = "#"
-# customer_car.color= "#"
-# customer.car = car.new
-
-  
